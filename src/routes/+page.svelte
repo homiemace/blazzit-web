3555958445812a526b23f6f136609918eaa0fabf
homie <90294477+homiemace@users.noreply.github.com>
2025-05-21 18:17:30 -0500

    Added routing and loading for posts.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+	const posts = data.posts ?? [];
+</script>
+
+{#if posts.length > 0}
+	{#each posts as post}
+		<div class="post">
+			<h2>{post.title}</h2>
+			<p>{post.content}</p>
+		</div>
+	{/each}
+{:else}
+	<div>No posts found.</div>
+{/if}
---
+++ src/routes/+page.ts
@@ -0,0 +1,9 @@
+import type { PageLoad } from './$types';
+import { supabase } from "$lib/supabaseClient";
+
+export async function load() {
+    const { data } = await supabase.from("posts").select();
+    return {
+      posts: data ?? [],
+    };
+  }
